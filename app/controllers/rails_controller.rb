3121cd3e4c70bb58beba797ea1259ecc851af752
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-03-14 08:31:53 +0000

    [Add]contoroller

---
+++ app/controllers/rails_controller.rb
@@ -0,0 +1,22 @@
+class RailsController < ApplicationController
+  def g
+  end
+
+  def controller
+  end
+
+  def admin/genres
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
