5357cdafa68eec456e273ef4a594683e4f647edb
EC2 Default User <ec2-user@ip-172-31-0-93.ap-northeast-1.compute.internal>
2024-03-17 09:49:26 +0000

    rioto

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,20 +1,26 @@
 class Admin::ItemsController < ApplicationController
+  
   def index
     @items = Item.page(params[:page]).per(10)
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    redirect_to admin_items_path(@item)
   end
 
   def show
+    @item = Item.find(params[:id])
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
 
   def update
+    redirect_to admin_items_path(@item)
   end
 end
