a10b92751c4a073b27024922d304caeb9bef4704
EC2 Default User <ec2-user@ip-172-31-32-108.ap-northeast-1.compute.internal>
2024-03-18 10:53:23 +0000

    権限名の変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 
-  devise_for :customers
-  devise_for :admin
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+}
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+}
   
   
   
