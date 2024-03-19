67dbe05608317338a7ae0fa6d1a8229b919ed4e8
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-03-19 08:31:39 +0000

    [Add]支払い方法

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,5 +1,6 @@
 class Public::OrdersController < ApplicationController
   def new
+    @order = Order.new
   end
 
   def confirm
@@ -9,11 +10,21 @@ class Public::OrdersController < ApplicationController
   end
 
   def create
+    order = Order.new(order_params)
+    order.save
+    # 注文確認画面へリダイレクト
+    redirect_to '/confirm'
   end
 
   def index
   end
 
   def show
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:postal_code, :address, :name)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,9 @@
 class Order < ApplicationRecord
+  belongs_to :customer
+  has_many :order_details
+  
+  # 支払い方法　0:クレジットカード,1:銀行振込
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  # 注文ステータス　0:入金待ち,1:入金確認,2:製作中,3:発送準備中,4:発送済み
+  enum status: { waiting: 0, payment: 1, production:2, preparation:3, delivered:4 }
 end
