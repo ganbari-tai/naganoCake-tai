36803806d976deff9f2fafa7dd3c8649d85214c4
EC2 Default User <ec2-user@ip-172-31-7-37.ap-northeast-1.compute.internal>
2024-03-20 17:14:47 +0000

    [Add]注文確認画面

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,5 +1,8 @@
 class OrderDetail < ApplicationRecord
   belongs_to :order
+  belongs_to :item
   
+  # 製作ステータス　0:製作不可,1:製作待ち,2:作製中,3:作製完了
+  enum making_status: { impossible_making: 0, waiting_making: 1, making: 2, finish: 3 }
 
 end
