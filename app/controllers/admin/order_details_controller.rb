fdd965621668272e6a9ed2aad35c3f53cf69f518
EC2 Default User <ranm358m@gmail.com>
2024-03-20 12:14:09 +0000

    [Update]admin/注文詳細画面

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,2 +1,9 @@
 class Admin::OrderDetailsController < ApplicationController
+
+  def update
+    @order_details = OrderDetails.find(params[:id])
+    @order_details.update
+    redirect_to admin_order_path
+  end
+  
 end
