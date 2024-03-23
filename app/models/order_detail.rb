47ac76ee89ce784343bde2870a8d858624337029
EC2 Default User <ranm358m@gmail.com>
2024-03-23 10:21:09 +0000

    [Update]enum_helpの導入、admin/order,order_detailの注文詳細、ステータス変更の追加

--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -5,4 +5,7 @@ class OrderDetail < ApplicationRecord
   # 製作ステータス　0:製作不可,1:製作待ち,2:作製中,3:作製完了
   enum making_status: { impossible_making: 0, waiting_making: 1, making: 2, finish: 3 }
 
+  def subtotal
+    item.add_tax_price * amount
+  end
 end
