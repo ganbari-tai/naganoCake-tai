025fe55c5077339a2f009d4cabd730fa08e43b80
EC2 Default User <ec2-user@ip-172-31-0-93.ap-northeast-1.compute.internal>
2024-03-25 02:40:39 +0000

    rioto

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,4 +20,5 @@ class Item < ApplicationRecord
   validates :name, presence: true
   validates :introduction, presence: true
   validates :genre_id, presence: true
+  validates :price, presence: true
 end
